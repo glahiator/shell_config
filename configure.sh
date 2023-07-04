893d7752b038ec15d16019072cab49644cfbb3d4
Rasul <rasul@glashev.ru>
2023-07-04 22:56:33 +0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -19,5 +19,7 @@ if [ "${OS}" = "Linux" ] ; then
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+        cp .zshrc ~/.zshrc
         
         
