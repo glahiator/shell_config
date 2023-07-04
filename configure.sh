83223352eab151f053b95849c5acc488f657fb3d
Rasul <rasul@glashev.ru>
2023-07-04 23:01:46 +0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -17,9 +17,12 @@ if [ "${OS}" = "Linux" ] ; then
         echo "export SHELL=/bin/zsh" > ~/.bash_profile
         echo "exec /bin/zsh -l" >> ~/.bash
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        exit
         git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
         cp .zshrc ~/.zshrc
+    fi
+fi
         
         
