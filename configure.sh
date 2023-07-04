2658c312a27f368e508bf340ee2701757f34db52
Rasul <rasul@glashev.ru>
2023-07-04 23:43:29 +0300

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -21,6 +21,7 @@ if [ "${OS}" = "Linux" ] ; then
         git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
         git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
         cp .zshrc ~/.zshrc
+        cp .p10k.zsh ~/.p10k.zsh
         zsh
     fi
 fi
